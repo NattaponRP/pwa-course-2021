597f208a931da2fa4a31eb2dc2967472f8a3e11c
NitithornRm <nitithorn.rutm@bumail.net>
2021-05-08 10:10:47 +0700

    ทำ cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,12 @@
 self.addEventListener("install", (event) => {
-    console.log("[SW] Service Worker is installing...");
+    console.log("[SW] Service Worker is installing... v1.2");
+    event.waitUntil(
+        caches.open('chatapp-v1.2').then((cache)=> {
+            return cache.addAll([
+            '/',
+            "/index.html"
+            ]);
+        })
+    )
 })
 
