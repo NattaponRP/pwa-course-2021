f52cd0eff8addcf5038ffce88bad6b531c06d412
NitithornRm <nitithorn.rutm@bumail.net>
2021-05-12 17:52:17 +0700

    เพิ่มปุ่ม home

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import shopButton from './shop_button.png'
 import friendButton from './friendList_button.png'
 import chatroomButton from './Chatbutton.png'
 import dropdownButton from './icon2.png'
+import homebutton from './homebutton.png'
 
 
 import './onClick.js'
@@ -22,9 +23,11 @@ function App({children}) {
             <button onclick="myFunction()" class="dropbtn"><img src={dropdownButton}></img></button>
               <div id="myDropdown" class="dropdown-content">
             
+                <a href="#"><Link to="/"><img src={homebutton}></img></Link></a>
+                <a href="#"><Link to="/chatbox"><img src={chatroomButton}></img></Link></a>
                 <a href="#"><Link to="/friend-list"> <img src={friendButton}></img> </Link></a>
                 <a href="#"><Link to="/shop"> <img src={shopButton}></img> </Link></a>
-                <a href="#"><Link to="/chatbox"><img src={chatroomButton}></img></Link></a>
+                
                 
               </div>
         </div>
